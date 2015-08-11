9eeb9680fc81ae20c2a09c493f64cb917b3187b9
Curtis Farnham <cfarnham@phone.com>
2015-08-10 18:27:17 -0700

    Updating MasonCollectionSchema since properties are moved out of @meta now

--- src/Schema/Contrib/MasonCollectionSchema.php
+++ src/Schema/Contrib/MasonCollectionSchema.php
@@ -3,7 +3,6 @@ namespace PhoneCom\Mason\Schema\Contrib;
 
 use PhoneCom\Mason\Schema\DocumentSchema;
 use PhoneCom\Mason\Builder\Contrib\MasonCollection;
-use Illuminate\Http\Request;
 
 class MasonCollectionSchema extends DocumentSchema
 {
@@ -12,15 +11,15 @@ class MasonCollectionSchema extends DocumentSchema
         parent::__construct($properties);
 
         $this
-            ->setMetaProperty('page', 'integer', ['title' => 'Page number', 'default' => 1])
-            ->setMetaProperty('page_size', 'integer', [
+            ->setRequiredProperty('page', 'integer', ['title' => 'Page number', 'default' => 1])
+            ->setRequiredProperty('page_size', 'integer', [
                 'title' => 'Maximum number of items per page',
                 'default' => MasonCollection::DEFAULT_PER_PAGE,
                 'maximum' => MasonCollection::MAX_PER_PAGE
             ])
-            ->setMetaProperty('total_pages', 'integer', [
+            ->setRequiredProperty('total_pages', 'integer', [
                 'title' => 'Total number of pages in the collection, if using the current page_size'
             ])
-            ->setMetaProperty('total_items', 'integer', ['title' => 'Total number of items in the collection']);
+            ->setRequiredProperty('total_items', 'integer', ['title' => 'Total number of items in the collection']);
     }
 }
